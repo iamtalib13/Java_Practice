333fcf7daf06f1acd1995860030b9e7e514c58c8
iamtalib13 <talibsheikh16@gmail.com>
2022-07-24 20:05:10 +0530

    Added Unary operator java program

---
+++ UnaryOperator.java
@@ -0,0 +1,11 @@
+public class UnaryOperator {
+    public static void main(String[] args) {
+        //Unary Operator
+        //++ & -- postInc & preInc ,PostDec & PreDec
+        int i = 6;
+        System.out.println("i value = " + i);
+        System.out.println("i value after pre increment = " + ++i);//7
+        System.out.println("i value after post increment = " + i++);//7
+        System.out.println("i value = " + i);
+    }
+}
