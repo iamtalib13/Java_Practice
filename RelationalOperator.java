0c138ed5c17d181c971d3c7d3fa306724a78cda8
iamtalib13 <talibsheikh16@gmail.com>
2022-07-24 19:58:43 +0530

    Added Relational operators java program

---
+++ RelationalOperator.java
@@ -0,0 +1,13 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+        // Relational operator == ,<=,>=, !=,>,<
+          int num1 = 20;
+            int num2 = 30;
+            System.out.println("Result = " + (num1 == num2));//false
+            System.out.println("Result = " + (num1 <= num2));//20 <= 30 =true
+            System.out.println("Result = " + (num1 >= num2));//20 >=30 =false
+            System.out.println("Result = " + (num1 != num2));//20 !=30 = true
+            System.out.println("Result = " + (num1 < num2));// 20 < 30 = Ture
+    }
+}
+
