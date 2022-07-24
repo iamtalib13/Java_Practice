42a1df66ce355ff14234e9d643625a0679c5fabc
iamtalib13 <talibsheikh16@gmail.com>
2022-07-24 20:02:31 +0530

    Added shift operators java program

---
+++ ShiftOperator.java
@@ -0,0 +1,9 @@
+public class ShiftOperator {
+    public static void main(String[] args) {
+        //left shift & right shift
+           int number = 4; //100 =4<<2 = 16 =100000
+            System.out.println("Left shift of number " + (number<<3));
+            System.out.println("Right shift of number " + (number>>2));//001
+    }
+}
+
