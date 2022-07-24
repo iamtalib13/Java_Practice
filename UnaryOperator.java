9a5878bb276026fd2bc94fe945631e1013a9d4f9
iamtalib13 <talibsheikh16@gmail.com>
2022-07-24 20:10:03 +0530

    Added Unary operator java program

--- UnaryOperator.java
+++ UnaryOperator.java
@@ -7,5 +7,12 @@ public class UnaryOperator {
         System.out.println("i value after pre increment = " + ++i);//7
         System.out.println("i value after post increment = " + i++);//7
         System.out.println("i value = " + i);
+
+
+        int j = 7;
+          System.out.println("j value = " + j);//7
+            System.out.println("j value after pre decrement = " + --j);//6
+            System.out.println("j value after post decrement = " + j--);//6
+            System.out.println("j value = " + j);//5
     }
 }
